a7c0ba8940b2b2ae0cdb4e44704969e00d52ce3b
chatbox9321 <85362220+chatbox9321@users.noreply.github.com>
2022-05-07 23:22:27 +0530

    Second commit

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
       <title>Anime List | Home</title>
       <meta name="keyword" content='anime' />
     </Head>
-    <div class={styles.title}>
-      <h1 class={styles.title}>Anime</h1>
+    <div className={styles.title}>
+      <h1 className={styles.title}>Anime</h1>
       <p>Anime (Japanese: アニメ, IPA: [aɲime] (listen)) is hand-drawn and computer-generated animation originating from Japan. Outside of Japan and in English, anime refers to Japanese animation, and refers specifically to animation produced in Japan.[1] However, in Japan and in Japanese, anime (a term derived from a shortening of the English word animation) describes all animated works, regardless of style or origin. Animation produced outside of Japan with similar style to Japanese animation is commonly referred to as anime-influenced animation.</p>
       <Link href="/about">
         <a>About us</a>
